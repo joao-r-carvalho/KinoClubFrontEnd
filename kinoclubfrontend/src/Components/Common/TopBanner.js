6e867b4e07d7a444597c6397a40980da3b6a4322
joao-r-carvalho <joao.r.carvalho@outlook.de>
2020-07-29 18:22:35 +0100

    Added API call with axios, added top banner

---
+++ kinoclubfrontend/src/Components/Common/TopBanner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../Style/TopBanner.css'
+import Clock from '../Common/Clock.js'
+
+function TopBanner() {
+    return (<div className="TopBanner">
+        <h1 >Movie Presentation! </h1>
+        <Clock />
+
+
+    </div>)
+
+}
+
+export default TopBanner;
--- kinoclubfrontend/src/Components/Movies/MoviePresentation.js
+++ kinoclubfrontend/src/Components/Movies/MoviePresentation.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import Clock from '../Common/Clock.js'
 import Spoilers from '../../Resources/Spoilers.jpg'
+import axios from 'axios';
+
 class MoviePresentation extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isImageHidden: true
+            isImageHidden: true,
+            error: null,
+            isLoaded: false,
+            MovieDescription: null,
+            MovieTitle: null,
+            MovieImageURL: null
         }
         this.ToggleImage = this.ToggleImage.bind(this);
 
     }
+    componentDidMount() {
+        axios.get("https://kinoclub-1595860726231.azurewebsites.net/Movies/Random")
+            .then(res => {
+                this.setState({
+                    isLoaded: true,
+                    MovieDescription: res.data.Description,
+                    MovieTitle: res.data.Title,
+                    MovieImageURL: res.data.Image,
+                })
+            }, (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                })
+            }
+            )
 
+    }
     ToggleImage() {
         this.setState(state => ({
             isImageHidden: !this.state.isImageHidden
@@ -18,29 +41,31 @@ class MoviePresentation extends React.Component {
 
     }
     render() {
-        return (
-            <div>
+        if (this.state.error) {
+            return <div>Error: {this.state.error.message}</div>;
+        } else if (!this.state.isLoaded) {
+            return <div>Loading... </div>;
+        } else {
+            return (
                 <div>
-                    <h1 >Movie Presentation! </h1>
-                    <Clock />
+
+                    <table>
+                        <tr>
+                            <td>
+                                <img src={this.state.isImageHidden ? Spoilers : this.state.MovieImageURL}
+                                    onClick={this.ToggleImage} >
+
+                                </img>
+                            </td>
+                            <td rowspan='3'>
+                                <h1> {this.state.MovieTitle} </h1>
+                                <p>{this.state.MovieDescription}</p>
+                            </td>
+                        </tr>
+                    </table>
                 </div>
-                <table>
-                    <tr>
-                        <td>
-                            <img src={this.state.isImageHidden ? Spoilers : this.props.MoviePosterURL}
-                                onClick={this.ToggleImage} >
-
-                            </img>
-                        </td>
-                        <td rowspan='3'>
-                            <h1> {this.props.MovieName} </h1>
-                            <p>{this.props.MovieDescription}</p>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        )
-
+            )
+        }
     }
 }
 
