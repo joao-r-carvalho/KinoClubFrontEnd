5c98475d794efeeb4a197cb7739ef8c88e2c4b92
joao-r-carvalho <joao.r.carvalho@outlook.de>
2020-07-30 15:17:24 +0100

    Updates

---
+++ precache-manifest.5c319860e5bdfb14b441d7fe0ad5be49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d50afd26a02ceeb0221eaa5d3c16f38",
+    "url": "/KinoClubFrontEnd/index.html"
+  },
+  {
+    "revision": "0bdc9998b63a78f08ab4",
+    "url": "/KinoClubFrontEnd/static/css/main.6e0002c9.chunk.css"
+  },
+  {
+    "revision": "b0e9938910d946002311",
+    "url": "/KinoClubFrontEnd/static/js/2.9b722e2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KinoClubFrontEnd/static/js/2.9b722e2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bdc9998b63a78f08ab4",
+    "url": "/KinoClubFrontEnd/static/js/main.254cad95.chunk.js"
+  },
+  {
+    "revision": "857954c525bedc2d495a",
+    "url": "/KinoClubFrontEnd/static/js/runtime-main.d02cb81c.js"
+  },
+  {
+    "revision": "0b7f3581659fe8d37e2bd3b0ea546d93",
+    "url": "/KinoClubFrontEnd/static/media/Spoilers.0b7f3581.jpg"
+  }
+]);
