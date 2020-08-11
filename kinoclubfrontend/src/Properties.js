7c01e07f5f1f4c6f982ae4d4f913546df82766ad
joao-r-carvalho <joao.r.carvalho@outlook.de>
2020-08-11 18:21:19 +0100

    Fixed props

--- kinoclubfrontend/src/Properties.js
+++ kinoclubfrontend/src/Properties.js
@@ -1,4 +1,4 @@
 export const properties = {
-    // BaseURL: "https://kinoclub-1595860726231.azurewebsites.net"
-     BaseURL: "http://localhost:8082"
+    BaseURL: "https://kinoclub-1595860726231.azurewebsites.net"
+    //  BaseURL: "http://localhost:8082"
 };
