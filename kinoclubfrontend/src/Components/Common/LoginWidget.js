72c06591d362506b1a0f19d563d1fdeef29b8716
joao-r-carvalho <joao.r.carvalho@outlook.de>
2020-08-13 16:58:37 +0100

    Changes to login widget

--- kinoclubfrontend/src/Components/Common/LoginWidget.js
+++ kinoclubfrontend/src/Components/Common/LoginWidget.js
@@ -1,39 +1,88 @@
 import React from 'react';
 import '../../Style/Common.css';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { properties } from '../../Properties.js'
+import { FcBusinessman, FcBusinesswoman } from 'react-icons/fc'
+import axios from 'axios'
 
 class LoginWidget extends React.Component {
     constructor(props) {
         super(props);
 
-        this.handleLoginClick = this.handleLoginClick.bind(this);
         this.handleLogoutClick = this.handleLogoutClick.bind(this);
         this.state = {
             isLoggedIn: false,
-            username: "João Carvalho"
+            isLoaded: false,
+            isMale: true
         }
     }
-    handleLoginClick() {
 
-        this.setState({ isLoggedIn: true });
-    }
+
     handleLogoutClick() {
-        this.setState ({
+        this.setState({
             isLoggedIn: false
         })
     }
-    componentDidMount
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.ProfilePooling(),
+            5000
+        );
+
+    }
+    ProfilePooling = () => {
+        if (!this.state.isLoggedIn) {
+            fetch(properties.BaseURL + '/Users/Me', {
+                method: 'GET',
+                credentials: 'include',
+                headers: {
+                    'Accept': '*/*',
+                    'Content-Type': 'application/json',
+                }
+            }).then((response) => {
+                if (response.status == 401) {
+                    this.setState({ isLoggedIn: false })
+                }
+                else {
+                    response.json()
+                        .then(res => {
+                            alert(JSON.stringify(res));
+
+                            this.setState({
+                                isLoggedIn: true
+                            });
+                            this.timerID = null;
+                        }
+                        );
+                }
+                this.setState({
+                    isLoaded: true
+                })
+
+            });
+
+        }
+
+    }
+
+
+
+
     render() {
         let body;
 
         if (this.state.isLoggedIn) {
-            body = <div><h2>Welcome {this.state.username}</h2>
+            body = <div>
+                {this.state.isMale ? <FcBusinessman size={48}> </FcBusinessman>
+                    : <FcBusinesswoman size={48}> </FcBusinesswoman>
+                }
+                <span>Logged in as : </span>
                 <button onClick={this.handleLogoutClick} className="KinoButton KinoButton-Secondary">Logout</button></div>;
 
         } else {
             body = <div>
                 <Link to={process.env.PUBLIC_URL + '/Login'} className="KinoButton KinoButton-Main" tag="button">Login</Link>
-                <Link to ={process.env.PUBLIC_URL + '/Register'} className="KinoButton KinoButton-Secondary">Register</Link></div>;
+                <Link to={process.env.PUBLIC_URL + '/Register'} className="KinoButton KinoButton-Secondary">Register</Link></div>;
         }
 
         return (
