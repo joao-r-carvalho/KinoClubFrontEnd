436065d96f3d3f52977369940dd5d2a05d5ead23
joao-r-carvalho <joao.r.carvalho@outlook.de>
2020-08-26 20:19:29 +0100

    Fix properties

--- kinoclubfrontend/src/Properties.js
+++ kinoclubfrontend/src/Properties.js
@@ -1,6 +1,6 @@
 export const properties = {
-    // BaseURL: "https://kinoclub-1595860726231.azurewebsites.net"
-     BaseURL: "http://localhost:8082",
+    BaseURL: "https://kinoclub-1595860726231.azurewebsites.net",
+    //  BaseURL: "http://localhost:8082",
      SetFavoriteMovieURL :  "Users/AddToFavorites",
      RemoveFavoriteMovieURL :  "Users/RemoveFromFavorites"
 
